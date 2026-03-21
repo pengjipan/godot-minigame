7858beb231cc8f1136c7bd059ccd806069a83954
pengjipan <408002317@qq.com>
2026-03-21 11:23:49 +0800

    Fix game_hud.tscn node hierarchy to match script expectations and add 3 playable characters

--- scenes/menus/character_select.gd
+++ scenes/menus/character_select.gd
@@ -5,15 +5,23 @@ class_name CharacterSelect
 @export var available_characters: Array[CharacterData] = []
 
 var selected_character: CharacterData = null
+var character_container: VBoxContainer = null
 
 func _ready() -> void:
+	# Get the container for character buttons
+	character_container = $PanelContainer/VBoxContainer/ScrollContainer/VBoxContainer
+
 	# Create character buttons
 	for i in range(available_characters.size()):
 		var char_data = available_characters[i]
 		var button = Button.new()
-		button.text = char_data.character_name
-		button.pressed.connect(_on_character_selected.bindv([char_data]))
-		add_child(button)
+		button.text = char_data.character_name + "\n" + char_data.description
+		button.custom_minimum_size = Vector2(0, 120)
+		button.pressed.connect(_on_character_selected.bind(char_data))
+		character_container.add_child(button)
+
+	# Connect back button
+	$PanelContainer/VBoxContainer/BackButton.pressed.connect(_on_back_pressed)
 
 func _on_character_selected(character: CharacterData) -> void:
 	selected_character = character
